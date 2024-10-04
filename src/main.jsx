9929ede94d9291e6ae533fb8ef3ed5dc75d65894
Alexander Braff <awb2987@gmail.com>
2024-10-04 04:07:28 -0400

    had to revert back in my main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './App.css'; 
 
 // CREATE a root for the React app
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// RENDER the app component wrapped in BrowserRouter for routing
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
