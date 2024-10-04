e81baae6cb3e28c04441a83a58287c74e414dcfb
Alexander Braff <awb2987@gmail.com>
2024-10-04 04:18:37 -0400

    moved index.html to src folder and updated vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react';
 // Define and export the Vite configuration
 export default defineConfig({
   plugins: [react()],
-  root: './',
+  root: 'src',
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     rollupOptions: {
-      input: 'index.html',
+      input: 'src/index.html',
     },
   },
 });
