46447e0bf686bec492489393cce143949ef3baf6
Alexander Braff <awb2987@gmail.com>
2024-10-04 07:04:29 -0400

    updated the links to the photos in portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import Project from '../components/Project';
-import projectPhoto1 from '/assets/images/project1.jpg';
-import projectPhoto2 from '/assets/images/project2.jpg';
-import projectPhoto3 from '/assets/images/project3.jpg';
-
+import projectImage1 from '../assets/images/project1.jpg';
+import projectImage2 from '../assets/images/project2.jpg';
+import projectImage3 from '../assets/images/project3.jpg';
 
 const projects = [
   {
     title: 'Stock-Up',
-    image: {projectPhoto1},
+    image: projectImage1,
     deployedLink: 'https://surpritam.github.io/stock-up/',
     githubLink: 'https://github.com/awb2987/stock-up',
   },
   {
     title: 'Purr-Pal',
-    image: {projectPhoto2},
+    image: projectImage2,
     deployedLink: 'https://catapp-pde6.onrender.com',
     githubLink: 'https://github.com/awb2987/CatApp',
   },
   {
     title: 'Plan 4 It',
-    image: {projectPhoto3},
+    image: projectImage3,
     deployedLink: 'https://plan-4-it-1.onrender.com/',
     githubLink: 'https://github.com/awb2987/plan-4-it',
   },
