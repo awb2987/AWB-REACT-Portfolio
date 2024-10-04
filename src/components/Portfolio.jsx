991db0f3e9023bfd045571f5ad72c53cf34e4b34
Alexander Braff <awb2987@gmail.com>
2024-10-04 06:29:01 -0400

    got everything working again, but for some reason the assets don't load when I deploy to netlify. I am going to try having the assets in public and deploy. If that does work then great

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import Project from '../components/Project';
-import project1Photo from '../assets/images/project1.jpg';
-import project2Photo from '../assets/images/project2.jpg';
-import project3Photo from '../assets/images/project3.jpg';
 
 const projects = [
   {
     title: 'Stock-Up',
-    image: project1Photo,
+    image: '/assets/images/project1.jpg',
     deployedLink: 'https://surpritam.github.io/stock-up/',
     githubLink: 'https://github.com/awb2987/stock-up',
   },
   {
     title: 'Purr-Pal',
-    image: project2Photo,
+    image: '/assets/images/project2.jpg',
     deployedLink: 'https://catapp-pde6.onrender.com',
     githubLink: 'https://github.com/awb2987/CatApp',
   },
   {
     title: 'Plan 4 It',
-    image: project3Photo,
+    image: '/assets/images/project3.jpg',
     deployedLink: 'https://plan-4-it-1.onrender.com/',
     githubLink: 'https://github.com/awb2987/plan-4-it',
   },
