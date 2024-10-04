6901d438a4ed8eeecfa5661b9e3fa7099f5083b1
Alexander Braff <awb2987@gmail.com>
2024-10-04 04:41:07 -0400

    altered photo and pdf paths and fixed dist in the vite.config.js file

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import myPhoto from '../assets/images/my-photo.jpg';
 
 const AboutMe = () => {
   return (
     <section className="about-me">
       <h2>About Me</h2>
-      <img src={myPhoto} alt="A portrait of Alexander Braff" className="profile-pic" />
+      <img src="/assets/images/my-photo.jpg" alt="A portrait of Alexander Braff" className="profile-pic" />
       <p>
         My React portfolio describes past projects during my time studying via UofT BOOTCAMP. 
         I am a beekeeper trying to implement code to benefit beekeepers worldwide through gathering 
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     open: true,
   },
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     rollupOptions: {
       input: 'src/index.html',
     },
