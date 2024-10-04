78ed7bfdd2ec5f636cdf44896d505db7caf89d6a
Alexander Braff <awb2987@gmail.com>
2024-10-03 23:46:57 -0400

    another adjustment of vite.config.js to try and get netlify to build and deploy

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react';
 // Define and export the Vite configuration
 export default defineConfig({
   plugins: [react()],
+  root: './',
   server: {
     port: 5173,
     open: true,
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+    },
   },
-  base: './',
 });
